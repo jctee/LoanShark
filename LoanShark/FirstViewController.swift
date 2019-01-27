5094c0c0e1875fc2ca004b1a41aea77b4c787db0
JC Tee <jctee@jcs-macbook-pro.lan>
2019-01-27 01:06:59 -0800

    Initial Commit

---
+++ LoanShark/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  LoanShark
+//
+//  Created by JC Tee on 2019-01-27.
+//  Copyright © 2019 JC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ LoanShark/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  LoanShark
+//
+//  Created by JC Tee on 2019-01-27.
+//  Copyright © 2019 JC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
